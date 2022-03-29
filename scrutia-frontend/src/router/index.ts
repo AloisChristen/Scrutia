8b95540104e14f29676d0d522a37c64c1e7d888b
Alexis-Allemann <alexis.allemann@gmail.com>
2022-03-29 13:15:00 +0200

    Tested to add a new route

--- scrutia-frontend/src/router/index.ts
+++ scrutia-frontend/src/router/index.ts
@@ -16,6 +16,11 @@ const routes: Array<RouteRecordRaw> = [
     component: () =>
       import(/* webpackChunkName: "about" */ "../views/AboutView.vue"),
   },
+  {
+    path: "/landing",
+    name: "landing",
+    component: () => import("../views/LandingView.vue"),
+  },
 ];
 
 const router = createRouter({
