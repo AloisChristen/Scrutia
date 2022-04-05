524aef7ca7aa729662dd09f72478d8614759df3f
Alexis-Allemann <alexis.allemann@gmail.com>
2022-04-05 16:22:25 +0200

    Added start script

--- scrutia-frontend/server.js
+++ scrutia-frontend/server.js
@@ -3,7 +3,7 @@ const serveStatic = require("serve-static");
 const path = require("path");
 const app = express();
 app.use("/", serveStatic(path.join(__dirname, "/dist")));
-app.get(/.*/, function (req, res) {
+app.get(/.*/, function (_, res) {
   res.sendFile(path.join(__dirname, "/dist/index.html"));
 });
 const port = process.env.PORT || 8080;
