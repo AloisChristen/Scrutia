4fc31be3e8743cdb21fcdfce2bd54f069ada210b
Alexandre Mottier <alex_mottier@hotmail.com>
2022-04-06 20:47:12 +0200

    Adding logout feature

--- scrutia-api/app/Http/Controllers/Auth/LoginController.php
+++ scrutia-api/app/Http/Controllers/Auth/LoginController.php
@@ -5,6 +5,8 @@ namespace App\Http\Controllers\Auth;
 use App\Http\Controllers\Controller;
 use App\Http\Requests\Auth\LoginRequest;
 use App\Models\User;
+use Illuminate\Http\JsonResponse;
+use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Hash;
 use Illuminate\Validation\ValidationException;
 
@@ -13,23 +15,36 @@ class LoginController extends Controller
     /**
      * Handle an incoming authentication request.
      *
-     * @param \App\Http\Requests\Auth\LoginRequest $request
-     * @return \Illuminate\Http\JsonResponse
+     * @param LoginRequest $request
+     * @return JsonResponse
      * @throws \Illuminate\Validation\ValidationException
      */
     public function login(LoginRequest $request)
     {
         $validated = $request->validated();
         if ($validated) {
-            $user = User::where('email', $request->email)->first();
+            $user = User::where('username', $request->username)->first();
 
-            if (! $user || ! Hash::check($request->password, $user->password)) {
-                throw ValidationException::withMessages([
-                    'email' => ['The provided credentials are incorrect.'],
-                ]);
+            if (!$user || ! Hash::check($request->password, $user->password)) {
+                return response()->json([
+                    'error' => 'Wrong credentials'
+                ], 401);
             }
         }
 
-        return response()->json(['token' => $user->createToken($request->header('User-Agent'))->plainTextToken]);
+        return response()->json([
+            'token' => $user->createToken($request->header('User-Agent'))->plainTextToken,
+            'user' => $user
+        ]);
+    }
+
+    /**
+     * Handle an incoming logout request.
+     *
+     * @return JsonResponse
+     */
+    public function logout(Request $request)
+    {
+        return auth()->user()->tokens()->where('tokenable_id', auth()->id() )->delete();
     }
 }
