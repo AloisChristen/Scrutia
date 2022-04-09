aafab8430dd09fb101df90ef7a0d5899d9f23c85
Alexis-Allemann <alexis.allemann@gmail.com>
2022-04-09 11:08:32 +0200

    Updated dependencies and added BootstrapVue

--- scrutia-frontend/src/App.vue
+++ scrutia-frontend/src/App.vue
@@ -3,8 +3,8 @@
     <router-link to="/">Home</router-link> |
     <router-link to="/about">About</router-link> |
     <router-link to="/landing">Landing</router-link>
+    <router-view />
   </nav>
-  <router-view />
 </template>
 
 <style lang="scss">
