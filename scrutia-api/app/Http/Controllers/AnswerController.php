cb6e480b3ca9a57db181f18064acd3c649e0f4a9
Hakim Balestrieri <hakim@mbp-de-hakim.einet.ad.eivd.ch>
2022-04-12 20:04:54 +0200

    new models freshly created but EMPTY for the moment

---
+++ scrutia-api/app/Http/Controllers/AnswerController.php
@@ -0,0 +1,86 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Http\Requests\StoreAnswerRequest;
+use App\Http\Requests\UpdateAnswerRequest;
+use App\Models\Answer;
+
+class AnswerController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function index()
+    {
+        //
+    }
+
+    /**
+     * Show the form for creating a new resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function create()
+    {
+        //
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     *
+     * @param  \App\Http\Requests\StoreAnswerRequest  $request
+     * @return \Illuminate\Http\Response
+     */
+    public function store(StoreAnswerRequest $request)
+    {
+        //
+    }
+
+    /**
+     * Display the specified resource.
+     *
+     * @param  \App\Models\Answer  $answer
+     * @return \Illuminate\Http\Response
+     */
+    public function show(Answer $answer)
+    {
+        //
+    }
+
+    /**
+     * Show the form for editing the specified resource.
+     *
+     * @param  \App\Models\Answer  $answer
+     * @return \Illuminate\Http\Response
+     */
+    public function edit(Answer $answer)
+    {
+        //
+    }
+
+    /**
+     * Update the specified resource in storage.
+     *
+     * @param  \App\Http\Requests\UpdateAnswerRequest  $request
+     * @param  \App\Models\Answer  $answer
+     * @return \Illuminate\Http\Response
+     */
+    public function update(UpdateAnswerRequest $request, Answer $answer)
+    {
+        //
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     *
+     * @param  \App\Models\Answer  $answer
+     * @return \Illuminate\Http\Response
+     */
+    public function destroy(Answer $answer)
+    {
+        //
+    }
+}
