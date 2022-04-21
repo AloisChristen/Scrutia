0f773e68b20c0603902c6526526b2f84e87436ba
Alexandre Mottier <alex_mottier@hotmail.com>
2022-04-21 10:58:54 +0200

    fix seeders

--- scrutia-api/database/seeders/DatabaseSeeder.php
+++ scrutia-api/database/seeders/DatabaseSeeder.php
@@ -4,6 +4,7 @@ namespace Database\Seeders;
 
 use App\Models\Project;
 use App\Models\Question;
+use App\Models\User;
 use App\Models\Version;
 use Illuminate\Database\Console\Seeds\WithoutModelEvents;
 use Illuminate\Database\Seeder;
@@ -15,14 +16,13 @@ class DatabaseSeeder extends Seeder
      *
      * @return void
      */
-    public function run()
+    public function run(): void
     {
-        Answer::factory(10)->create();
-        Project::factory(10)->create();
-        Question::factory(10)->create();
-        Tag::factory(10)->create();
-        Version::factory(10)->create();
-        // \App\Models\User::factory(10)->create();
-        // QUESTION: why not making user seeder specific ??
+        User::factory(10)->create();
+        $this->call(AnswerSeeder::class);
+        $this->call(ProjectSeeder::class);
+        $this->call(QuestionSeeder::class);
+        $this->call(TagSeeder::class);
+        $this->call(VersionSeeder::class);
     }
 }
