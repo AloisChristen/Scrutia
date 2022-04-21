26c29f0fc4d98cf34723d0c9ebd6ab5bdfec242f
Alexandre Mottier <alex_mottier@hotmail.com>
2022-04-21 19:35:57 +0200

    Removing unneccessary code

--- scrutia-api/app/Models/Status.php
+++ scrutia-api/app/Models/Status.php
@@ -2,15 +2,9 @@
 
 namespace App\Models;
 
-use Illuminate\Support\Arr;
 
 enum Status: string
 {
     case IDEE = 'idee';
     case INITIATIVE = 'initiative';
-
-    public static function getRandomValue(): string
-    {
-        return Arr::random(Status::cases());
-    }
 }
