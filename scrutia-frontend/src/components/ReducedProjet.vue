4c8c6a0675765dbffbcaf6f05e704b1a8bf0c2e3
Alexis-Allemann <alexis.allemann@gmail.com>
2022-04-26 20:48:26 +0200

    Started created the reduced project component

---
+++ scrutia-frontend/src/components/ReducedProjet.vue
@@ -0,0 +1,17 @@
+<template>
+  <base-block :title="project.title" subtitle="" header-bg>
+    <p>{{ project.description }}</p>
+  </base-block>
+</template>
+
+<script>
+export default {
+  name: 'ReducedProjet',
+  props: {
+    project: {
+      type: Object,
+      description: 'The project to display',
+    },
+  },
+}
+</script>
--- scrutia-frontend/src/views/pages/Home.vue
+++ scrutia-frontend/src/views/pages/Home.vue
@@ -3,6 +3,26 @@
     <div class="content">
       <h1 class="content-heading">Accueil</h1>
       <p>...</p>
+      <b-row>
+        <b-col md="6" xl="3">
+          <reduced-projet
+            v-bind:project="{
+              title: 'Super titre',
+              description: 'Description de mon projet',
+            }"
+          />
+        </b-col>
+      </b-row>
     </div>
   </div>
 </template>
+
+<script>
+import ReducedProjet from '../../components/ReducedProjet.vue'
+
+export default {
+  components: {
+    ReducedProjet,
+  },
+}
+</script>
