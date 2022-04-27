6e46e18f4776d3c2a4e8b9b3bd509b7fc350a52c
Alexis-Allemann <alexis.allemann@gmail.com>
2022-04-27 10:57:06 +0200

    Added component on the home page

--- scrutia-frontend/src/views/pages/Home.vue
+++ scrutia-frontend/src/views/pages/Home.vue
@@ -1,28 +1,68 @@
 <template>
   <div>
     <div class="content">
-      <h1 class="content-heading">Accueil</h1>
-      <p>...</p>
+      <h2 class="content-heading">Idées les plus tendances...</h2>
+
       <b-row>
-        <b-col md="6" xl="3">
+        <b-col sm="12" md="4" xl="4" v-for="index in 6" :key="index">
+          <reduced-idea
+            v-bind:project="{
+              title: 'Sauver les pandas en Asie',
+              description:
+                'Description de mon projet. Ce texte peut parfois être super long. Ce texte peut parfois être super long. Ce texte peut parfois être super long.',
+            }"
+            :evenIndex="index % 2 == 0"
+          />
+        </b-col>
+      </b-row>
+      <p>
+        <b-link
+          class="link-fx"
+          href="javascript:void(0)"
+          style="float: right; margin-bottom: 10px; margin-top: -10px"
+          >et 125 autres...</b-link
+        >
+      </p>
+      <h2 class="content-heading">Projets d'initiative les plus actifs...</h2>
+      <b-row>
+        <b-col sm="12" md="6" xl="6" v-for="index in 4" :key="index">
           <reduced-projet
             v-bind:project="{
-              title: 'Super titre',
-              description: 'Description de mon projet',
+              title: 'Sauver les pandas en Asie',
+              description:
+                'Description de mon projet. Ce texte peut parfois être super long. Ce texte peut parfois être super long. Ce texte peut parfois être super long.',
             }"
           />
         </b-col>
       </b-row>
+      <p>
+        <b-link
+          class="link-fx"
+          href="javascript:void(0)"
+          style="float: right; margin-bottom: 10px; margin-top: -10px"
+          >et 28 autres...</b-link
+        >
+      </p>
     </div>
   </div>
 </template>
 
+<style>
+.block-title,
+.block-header,
+.block-content {
+  font-size: 12px;
+}
+</style>
+
 <script>
 import ReducedProjet from '../../components/ReducedProjet.vue'
+import ReducedIdea from '../../components/ReducedIdea.vue'
 
 export default {
   components: {
     ReducedProjet,
+    ReducedIdea,
   },
 }
 </script>
