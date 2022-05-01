08dad0fee5bc56c09d34139aafe2a09a09673958
Alexandre Mottier <alex_mottier@hotmail.com>
2022-05-01 15:20:19 +0200

    Enforce the assert on the test

--- scrutia-api/tests/Feature/QuestionTest.php
+++ scrutia-api/tests/Feature/QuestionTest.php
@@ -8,12 +8,11 @@ use App\Models\Status;
 use App\Models\User;
 use App\Models\Version;
 use Illuminate\Foundation\Testing\RefreshDatabase;
-use Illuminate\Foundation\Testing\WithFaker;
 use Tests\TestCase;
 
 class QuestionTest extends TestCase
 {
-    use RefreshDatabase, WithFaker;
+    use RefreshDatabase;
     /**
      * A basic feature test example.
      *
@@ -21,6 +20,7 @@ class QuestionTest extends TestCase
      */
     public function test_user_can_create_question(): void
     {
+        // As the database is refreshing, we're creating the ressources that we need to ask a question
         $user = User::factory()->create();
         $project = Project::create([
             "title" => "Test"
@@ -28,13 +28,13 @@ class QuestionTest extends TestCase
         $project->user()->associate($user);
         $version = Version::create([
             'number' => 1,
-            'status' => Status::INITIATIVE,
+            'status' => Status::IDEE,
             'description' => "Test",
-            'project_id' => $project->id,
-            'user_id' => $project->user->id,
         ]);
         $version->project()->associate($project);
+        $version->user()->associate($project->user);
 
+        // Making the request to the endpoint
         $response =
             $this->actingAs($user)
             ->post('/api/questions', [
@@ -43,7 +43,12 @@ class QuestionTest extends TestCase
                 "project_id" => $version->project->id,
                 "version_number" => $version->number,
             ]);
-        $response->assertCreated();
 
+        $response->assertCreated();
+        $this->assertEquals('"Created"', $response->getContent());
+        $this->assertDatabaseHas("questions", [
+            "title" => "test",
+            "description" => "Lorem Ipsum"
+        ]);
     }
 }
