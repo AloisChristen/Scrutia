7fa8eb6ea300d8617e54f3c1a53bac39eced482f
Alexis-Allemann <alexis.allemann@gmail.com>
2022-05-04 20:52:22 +0200

    Advanced landing page

--- scrutia-frontend/src/views/pages/Landing.vue
+++ scrutia-frontend/src/views/pages/Landing.vue
@@ -1,8 +1,82 @@
 <template>
   <div>
-    <div class="content">
-      <h1 class="content-heading">A propos</h1>
-      <p>...</p>
+    <div class="content content-full content-boxed">
+      <div class="py-5">
+        <b-row class="mb-5">
+          <b-col md="12">
+            <h2 class="h1 font-w700 mb-3">
+              Scrutia, <span class="font-w400">de l'idée au scrutin !</span>
+            </h2>
+            <p class="font-size-lg text-muted mb-3">
+              L’application Scrutia permet d’aider les personnes souhaitant
+              faire part d’une idée à pouvoir la concrétiser politiquement et la
+              promouvoir en initiative populaire. Il se veut aussi un site
+              communautaire permettant à tout un chacun de participer au cycle
+              de vie d’une initiative, de l’idée de base jusqu’au texte de loi
+              soumis à la Confédération.
+            </p>
+            <p class="font-size-lg text-muted mb-3">
+              Une idée pourra ainsi rassembler du soutien auprès des membres et
+              mûrir jusqu’à une forme finale. Celle-ci deviendra un projet
+              d’initiative concret. Ce projet aura l’avantage d’avoir déjà une
+              communauté le soutenant ainsi qu’un travail de réflexion sur la
+              forme et le fond.
+            </p>
+            <p class="font-size-lg text-muted mb-3">
+              Le principe de communauté est mis en avant afin de pouvoir
+              s’exprimer librement sur une idée, de proposer des versions
+              différentes du texte et de faire vivre la discussion au travers de
+              questions/réponses. La communauté est en partie auto-gérée, avec
+              un système de réputation permettant de débloquer de nouvelles
+              fonctionnalités pour les utilisateurs reconnus.
+            </p>
+            <p class="font-size-lg text-muted mb-3">
+              Cette application se veut un point d’entrée facile dans la vie
+              politique suisse, en permettant aux novices de participer aux
+              discussions des initiatives futures ou de proposer leurs idées.
+              Grâce aux propositions et à la recherche, un utilisateur trouvera
+              rapidement les sujets qui l’intéressent et auxquels il souhaite
+              apporter son aide.
+            </p>
+            <p class="font-size-lg text-muted mb-0">
+              La création d’initiatives se voit également accélérée de part la
+              communauté présente et les outils mis à disposition. Au travers
+              des versions, les initiatives s’amélioreront jusqu’à aboutir à des
+              projets finaux de qualité.
+            </p>
+          </b-col>
+        </b-row>
+      </div>
+    </div>
+
+    <!-- Stats -->
+    <div class="bg-primary">
+      <div class="bg-black-75">
+        <div class="content content-full content-boxed">
+          <div class="py-5 text-center text-white">
+            <h2 class="font-w700 mb-4">
+              Qui sommes-nous <i class="fa fa-fw fa-question text-city"></i>
+            </h2>
+            <p class="font-size-lg text-white-50 mb-2">
+              Nous sommes 5 étudiants en dernière année du Bachelor of Science
+              en Ingénierie logicielle.
+            </p>
+            <p class="font-size-lg text-white-50 mb-2">
+              Durant le 2ème semestre de l’année 2021-2022, nous devons réaliser
+              un travail de groupe.
+            </p>
+            <p class="font-size-lg text-white-50 mb-2">
+              Ce projet consiste à développer une application selon le processus
+              suivant :
+            </p>
+            <p class="font-size-lg text-white-50 mb-2">
+              Créer le cahier des charges / Prototypage de la solution / Mise en
+              place d’un environnement de développement adéquat / Création des
+              pipelines d’intégrations / Développer la solution
+            </p>
+          </div>
+        </div>
+      </div>
     </div>
   </div>
 </template>
