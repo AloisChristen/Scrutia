215c6d922198ab60c580c17017627ea7baab03f2
Alexis-Allemann <alexis.allemann@gmail.com>
2022-05-05 18:02:48 +0200

    [DONE] - Landing page

--- scrutia-frontend/src/views/pages/Landing.vue
+++ scrutia-frontend/src/views/pages/Landing.vue
@@ -2,12 +2,12 @@
   <div>
     <div class="content content-full content-boxed">
       <div class="py-5">
-        <b-row class="mb-5">
+        <b-row class="mb-3">
           <b-col md="12">
-            <h2 class="h1 font-w700 mb-3">
+            <h2 class="h1 font-w700 mb-4 text-center">
               Scrutia, <span class="font-w400">de l'idée au scrutin !</span>
             </h2>
-            <p class="font-size-lg text-muted mb-3">
+            <p class="font-size-md text-muted mb-3">
               L’application Scrutia permet d’aider les personnes souhaitant
               faire part d’une idée à pouvoir la concrétiser politiquement et la
               promouvoir en initiative populaire. Il se veut aussi un site
@@ -15,14 +15,14 @@
               de vie d’une initiative, de l’idée de base jusqu’au texte de loi
               soumis à la Confédération.
             </p>
-            <p class="font-size-lg text-muted mb-3">
+            <p class="font-size-md text-muted mb-3">
               Une idée pourra ainsi rassembler du soutien auprès des membres et
               mûrir jusqu’à une forme finale. Celle-ci deviendra un projet
               d’initiative concret. Ce projet aura l’avantage d’avoir déjà une
               communauté le soutenant ainsi qu’un travail de réflexion sur la
               forme et le fond.
             </p>
-            <p class="font-size-lg text-muted mb-3">
+            <p class="font-size-md text-muted mb-3">
               Le principe de communauté est mis en avant afin de pouvoir
               s’exprimer librement sur une idée, de proposer des versions
               différentes du texte et de faire vivre la discussion au travers de
@@ -30,7 +30,7 @@
               un système de réputation permettant de débloquer de nouvelles
               fonctionnalités pour les utilisateurs reconnus.
             </p>
-            <p class="font-size-lg text-muted mb-3">
+            <p class="font-size-md text-muted mb-3">
               Cette application se veut un point d’entrée facile dans la vie
               politique suisse, en permettant aux novices de participer aux
               discussions des initiatives futures ou de proposer leurs idées.
@@ -38,7 +38,7 @@
               rapidement les sujets qui l’intéressent et auxquels il souhaite
               apporter son aide.
             </p>
-            <p class="font-size-lg text-muted mb-0">
+            <p class="font-size-md text-muted mb-0">
               La création d’initiatives se voit également accélérée de part la
               communauté présente et les outils mis à disposition. Au travers
               des versions, les initiatives s’amélioreront jusqu’à aboutir à des
@@ -48,32 +48,133 @@
         </b-row>
       </div>
     </div>
-
-    <!-- Stats -->
+    <div class="bg-white">
+      <div class="content content-full content-boxed">
+        <div class="py-5">
+          <b-row class="mb-5">
+            <b-col md="6">
+              <h2 class="h1 font-w700 mb-2">Processus</h2>
+              <p class="font-size-lg font-w500 text-muted mb-0">
+                Découvrez comment notre plateforme fonctionne
+              </p>
+            </b-col>
+            <b-col
+              md="6"
+              class="d-none d-md-flex align-items-md-center justify-content-md-end"
+            >
+              <p class="h1 font-w700 text-body-bg-dark mb-0">Pour tous.</p>
+            </b-col>
+          </b-row>
+          <b-row>
+            <b-col sm="6" md="4" xl="3">
+              <div class="item item-rounded bg-gray-light my-4">
+                <i class="fa fa-2x fa-lightbulb text-dark"></i>
+              </div>
+              <h4 class="mb-2">Les idées</h4>
+              <p class="text-muted">
+                Chaque personne connectée au site peut proposer ses idées pour
+                améliorer la Constitution Suisse. Une idée peut être soutenue et
+                discutée par la communauté, offrant une place de débats et de
+                questions/réponses sur les thèmes importants.
+              </p>
+            </b-col>
+            <b-col sm="6" md="4" xl="3">
+              <div class="item item-rounded bg-info-light my-4">
+                <i class="fa fa-2x fa-rocket text-info"></i>
+              </div>
+              <h4 class="mb-2">Projet d'initiative</h4>
+              <p class="text-muted">
+                Lorsqu’une idée récolte assez de votes, elle passe au stade de
+                projet. Ici, elle passera par plusieurs versions, afin d’aboutir
+                au meilleur texte d’initiative possible. Les autres utilisateurs
+                peuvent proposer des versions ou soutenir celle qui leur plait
+                le plus.
+              </p>
+            </b-col>
+            <b-col sm="6" md="4" xl="3">
+              <div class="item item-rounded bg-success-light my-4">
+                <i class="fa fa-2x fa-comment-dots text-success"></i>
+              </div>
+              <h4 class="mb-2">Discussions</h4>
+              <p class="text-muted">
+                Discutez de vos idées ou des idées des autres. Poser des
+                questions améliorera votre réputation sur le site. C’est en
+                discutant que les meilleurs idées émergent.
+              </p>
+            </b-col>
+            <b-col sm="6" md="4" xl="3">
+              <div class="item item-rounded bg-warning-light my-4">
+                <i class="fa fa-2x fa-hand-point-right text-warning"></i>
+              </div>
+              <h4 class="mb-2">Vous êtes acteur</h4>
+              <p class="text-muted">
+                C’est vous qui choisissez ! Donnez votre soutien aux idées qui
+                vous plaisent. Vous pouvez aussi choisir de ne pas soutenir une
+                idée. Grâce à vous, la politique Suisse changera dans votre sens
+                !
+              </p>
+            </b-col>
+          </b-row>
+        </div>
+      </div>
+    </div>
     <div class="bg-primary">
       <div class="bg-black-75">
         <div class="content content-full content-boxed">
-          <div class="py-5 text-center text-white">
-            <h2 class="font-w700 mb-4">
-              Qui sommes-nous <i class="fa fa-fw fa-question text-city"></i>
-            </h2>
-            <p class="font-size-lg text-white-50 mb-2">
-              Nous sommes 5 étudiants en dernière année du Bachelor of Science
-              en Ingénierie logicielle.
-            </p>
-            <p class="font-size-lg text-white-50 mb-2">
-              Durant le 2ème semestre de l’année 2021-2022, nous devons réaliser
-              un travail de groupe.
-            </p>
-            <p class="font-size-lg text-white-50 mb-2">
-              Ce projet consiste à développer une application selon le processus
-              suivant :
-            </p>
-            <p class="font-size-lg text-white-50 mb-2">
-              Créer le cahier des charges / Prototypage de la solution / Mise en
-              place d’un environnement de développement adéquat / Création des
-              pipelines d’intégrations / Développer la solution
-            </p>
+          <div class="py-5">
+            <div class="text-center">
+              <h2 class="font-w700 mb-4 text-white">
+                Qui sommes-nous <i class="fa fa-fw fa-question text-city"></i>
+              </h2>
+              <p class="font-size-md text-white-50 mb-2">
+                Nous sommes 5 étudiants en dernière année du Bachelor of Science
+                en Ingénierie logicielle.
+              </p>
+              <p class="font-size-md text-white-50 mb-2">
+                Durant le 2ème semestre de l’année 2021-2022, nous devons
+                réaliser un travail de groupe.
+              </p>
+              <p class="font-size-md text-white-50 mb-2">
+                Ce projet consiste à développer une application selon le
+                processus suivant :
+              </p>
+            </div>
+            <b-container class="bv-example-row">
+              <b-row class="justify-content-sm-center">
+                <ul class="fa-ul">
+                  <li class="text-white-50 font-size-md mb-2">
+                    <span class="fa-li">
+                      <i class="fa fa-arrow-right"></i>
+                    </span>
+                    Créer le cahier des charges
+                  </li>
+                  <li class="text-white-50 font-size-md mb-2">
+                    <span class="fa-li">
+                      <i class="fa fa-arrow-right"></i>
+                    </span>
+                    Prototypage de la solution
+                  </li>
+                  <li class="text-white-50 font-size-md mb-2">
+                    <span class="fa-li">
+                      <i class="fa fa-arrow-right"></i>
+                    </span>
+                    Mise en place ce l'environnement de développement
+                  </li>
+                  <li class="text-white-50 font-size- mb-2">
+                    <span class="fa-li">
+                      <i class="fa fa-arrow-right"></i>
+                    </span>
+                    Création des pipelines d’intégrations
+                  </li>
+                  <li class="text-white-50 font-size-md">
+                    <span class="fa-li">
+                      <i class="fa fa-arrow-right"></i>
+                    </span>
+                    Développer la solution
+                  </li>
+                </ul>
+              </b-row>
+            </b-container>
           </div>
         </div>
       </div>
