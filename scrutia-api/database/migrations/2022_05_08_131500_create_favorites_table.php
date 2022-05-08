9072ce21939492615d74d163005f136e51f6675f
Alexandre Mottier <alex_mottier@hotmail.com>
2022-05-08 17:28:12 +0200

    add table and relationships

---
+++ scrutia-api/database/migrations/2022_05_08_131500_create_favorites_table.php
@@ -0,0 +1,35 @@
+<?php
+
+use App\Models\Project;
+use App\Models\User;
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('favorites', function (Blueprint $table) {
+            $table->id();
+            $table->foreignIdFor(User::class)->onDelete("cascade")->nullable();
+            $table->foreignIdFor(Project::class)->onDelete("cascade")->nullable();
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('favorites');
+    }
+};
