d5a094cd18288093ad3f893d84dedc0952ba2d38
Alex <alex_mottier@hotmail.com>
2022-05-10 20:48:47 +0200

    Remove validation for the moment

--- scrutia-api/app/Http/Controllers/Auth/RegistrationController.php
+++ scrutia-api/app/Http/Controllers/Auth/RegistrationController.php
@@ -18,24 +18,19 @@ class RegistrationController extends Controller
      */
     public function register(RegistrationRequest $request): JsonResponse
     {
-        $validated = $request->validated();
-        if ($validated) {
-            $user = User::create([
-                'username' => $request->username,
-                'firstname' => $request->firstname,
-                'lastname' => $request->lastname,
-                'email' => $request->email,
-                'password' => Hash::make($request->password),
-            ]);
+        // TODO : Check unique to respond with errors if already exists
+        $user = User::create([
+            'username' => $request->username,
+            'firstname' => $request->firstname,
+            'lastname' => $request->lastname,
+            'email' => $request->email,
+            'password' => Hash::make($request->password),
+        ]);
 
-            return response()->json([
-                'token' => $user->createToken($request->header('User-Agent'))->plainTextToken,
-                'user' => $user
-            ], 201);
-        }
         return response()->json([
-            'title' => 'Error: Bad Request',
-            'description' => 'Error during registration'
-        ], 401);
+            'token' => $user->createToken($request->header('User-Agent'))->plainTextToken,
+            'user' => $user
+        ], 201);
     }
+
 }
