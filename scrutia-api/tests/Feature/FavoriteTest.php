998842b5e8ec72357308147caf5688a4912fb307
Alex <alex_mottier@hotmail.com>
2022-05-10 20:31:26 +0200

    Add tests

---
+++ scrutia-api/tests/Feature/FavoriteTest.php
@@ -0,0 +1,36 @@
+<?php
+
+namespace Tests\Feature;
+
+use Illuminate\Foundation\Testing\RefreshDatabase;
+use Illuminate\Foundation\Testing\WithFaker;
+use Tests\TestCase;
+
+class FavoriteTest extends TestCase
+{
+    /**
+     * A basic feature test example.
+     *
+     * @return void
+     */
+    public function test_unauthenticated_user_cannot_add_project_to_favorites()
+    {
+        
+    }
+
+    public function test_user_can_add_project_to_favorites()
+    {
+
+    }
+
+    public function test_user_can_add_a_project_to_favorites_only_one_time()
+    {
+
+    }
+
+    public function test_user_cannot_add_non_existent_project_to_favorites()
+    {
+
+    }
+
+}
