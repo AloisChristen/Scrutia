845470b15bf1a5e7b38da68fe3fdaf13fb221a96
Alexandre Mottier <alex_mottier@hotmail.com>
2022-05-11 21:39:34 +0200

    finish testing

--- scrutia-api/tests/Feature/FavoriteTest.php
+++ scrutia-api/tests/Feature/FavoriteTest.php
@@ -36,7 +36,15 @@ class FavoriteTest extends TestCase
      */
     public function test_user_can_list_his_favorites(): void
     {
+        $user = User::factory()->create();
+        $project = Project::factory()->create();
 
+        $user->favorites()->save($project);
+
+        $response = $this->actingAs($user)->get("api/favorites");
+
+        $response->assertSuccessful();
+        $this->assertNotEmpty($response->getContent());
     }
 
     /**
@@ -57,7 +65,12 @@ class FavoriteTest extends TestCase
         $this->assertDatabaseCount("project_user", 1);
     }
 
-    public function test_user_can_add_a_project_to_favorites_only_one_time()
+    /**
+     * Test user can add a project on favorite only one time
+     *
+     * @return void
+     */
+    public function test_user_can_add_a_project_to_favorites_only_one_time(): void
     {
         $project = Project::factory()->create();
         $user = User::factory()->create();
@@ -76,18 +89,57 @@ class FavoriteTest extends TestCase
         $this->assertDatabaseCount("project_user", 1);
     }
 
-    public function test_user_cannot_add_non_existent_project_to_favorites()
+    /**
+     * Test user cannnot add a non existent project to his favorites
+     *
+     * @return void
+     */
+    public function test_user_cannot_add_non_existent_project_to_favorites(): void
     {
+        $user = User::factory()->create();
 
+        $response = $this->actingAs($user)->post("/api/favorites");
+
+        $response->assertNotFound();
+        $this->assertDatabaseCount("project_user",0);
     }
 
+    /**
+     * Test user can delete a favorite
+     *
+     * @return void
+     */
     public function test_user_can_delete_favorite(): void
     {
+        $user = User::factory()->create();
+        $project = Project::factory()->create();
 
+        $user->favorites()->save($project);
+        $this->assertEquals(1, $user->favorites()->count());
+
+        $response = $this->actingAs($user)->delete("/api/favorites/".$project->id);
+        $user = User::find($user->id);
+        $response->assertSuccessful();
+        $this->assertEquals(0, $user->favorites()->count());
     }
 
+    /**
+     * Test user cannot delete a favorite that does not exist
+     *
+     * @return void
+     */
     public function test_user_cannot_delete_favorite_of_non_existent_project(): void
     {
+        $user = User::factory()->create();
+        $project = Project::factory()->create();
 
+        $user->favorites()->save($project);
+        $this->assertEquals(1, $user->favorites()->count());
+
+        $response = $this->actingAs($user)->delete("/api/favorites/999");
+        $user = User::find($user->id);
+
+        $response->assertNotFound();
+        $this->assertEquals(1, $user->favorites()->count());
     }
 }
