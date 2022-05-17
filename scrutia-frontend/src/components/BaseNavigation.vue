7f975588b0743f5d2b6ab7bbbdc6d7e4d3aed438
Alexis-Allemann <alexis.allemann@gmail.com>
2022-05-17 20:37:24 +0200

    fixed bug with unused dispatch actions in store

--- scrutia-frontend/src/components/BaseNavigation.vue
+++ scrutia-frontend/src/components/BaseNavigation.vue
@@ -6,7 +6,10 @@
       :class="{
         'nav-main-heading': node.heading,
         'nav-main-item': !node.heading,
-        'open': node.sub && node.subActivePaths ? subIsActive(node.subActivePaths) : false
+        open:
+          node.sub && node.subActivePaths
+            ? subIsActive(node.subActivePaths)
+            : false,
       }"
     >
       {{ node.heading ? node.name : '' }}
@@ -14,14 +17,21 @@
         v-if="!node.heading && !node.sub"
         :to="node.to || '#'"
         class="nav-main-link"
-        :active-class="(node.to && node.to !== '#') ? 'active' : ''"
+        :active-class="node.to && node.to !== '#' ? 'active' : ''"
         @click.native="linkClicked($event)"
       >
         <i v-if="node.icon" :class="`nav-main-link-icon ${node.icon}`"></i>
         <span v-if="node.name" class="nav-main-link-name">{{ node.name }}</span>
-        <span v-if="node.badge" class="nav-main-link-badge badge badge-pill badge-primary"
-            :class="node['badge-variant'] ? `badge-${node['badge-variant']}` : 'badge-primary' "
-        >{{ node.badge }}</span>
+        <span
+          v-if="node.badge"
+          class="nav-main-link-badge badge badge-pill badge-primary"
+          :class="
+            node['badge-variant']
+              ? `badge-${node['badge-variant']}`
+              : 'badge-primary'
+          "
+          >{{ node.badge }}</span
+        >
       </router-link>
       <a
         v-else-if="!node.heading && node.sub"
@@ -31,11 +41,22 @@
       >
         <i v-if="node.icon" :class="`nav-main-link-icon ${node.icon}`"></i>
         <span v-if="node.name" class="nav-main-link-name">{{ node.name }}</span>
-        <span v-if="node.badge" class="nav-main-link-badge badge badge-pill badge-primary"
-            :class="node['badge-variant'] ? `badge-${node['badge-variant']}` : 'badge-primary' "
-        >{{ node.badge }}</span>
+        <span
+          v-if="node.badge"
+          class="nav-main-link-badge badge badge-pill badge-primary"
+          :class="
+            node['badge-variant']
+              ? `badge-${node['badge-variant']}`
+              : 'badge-primary'
+          "
+          >{{ node.badge }}</span
+        >
       </a>
-      <base-navigation v-if="node.sub" :nodes="node.sub" sub-menu></base-navigation>
+      <base-navigation
+        v-if="node.sub"
+        :nodes="node.sub"
+        sub-menu
+      ></base-navigation>
     </li>
   </ul>
 </template>
@@ -46,41 +67,41 @@ export default {
   props: {
     nodes: {
       type: Array,
-      description: 'The nodes of the navigation'
+      description: 'The nodes of the navigation',
     },
     subMenu: {
       type: Boolean,
       default: false,
-      description: 'If true, a submenu will be rendered'
+      description: 'If true, a submenu will be rendered',
     },
     dark: {
       type: Boolean,
       default: false,
-      description: 'Dark mode for menu'
+      description: 'Dark mode for menu',
     },
     horizontal: {
       type: Boolean,
       default: false,
-      description: 'Horizontal menu in large screen width'
+      description: 'Horizontal menu in large screen width',
     },
     horizontalHover: {
       type: Boolean,
       default: false,
-      description: 'Hover mode for horizontal menu'
+      description: 'Hover mode for horizontal menu',
     },
     horizontalCenter: {
       type: Boolean,
       default: false,
-      description: 'Center mode for horizontal menu'
+      description: 'Center mode for horizontal menu',
     },
     horizontalJustify: {
       type: Boolean,
       default: false,
-      description: 'Justify mode for horizontal menu'
-    }
+      description: 'Justify mode for horizontal menu',
+    },
   },
   computed: {
-    classContainer () {
+    classContainer() {
       return {
         'nav-main': !this.subMenu,
         'nav-main-submenu': this.subMenu,
@@ -88,21 +109,24 @@ export default {
         'nav-main-horizontal': this.horizontal,
         'nav-main-hover': this.horizontalHover,
         'nav-main-horizontal-center': this.horizontalCenter,
-        'nav-main-horizontal-justify': this.horizontalJustify
+        'nav-main-horizontal-justify': this.horizontalJustify,
       }
-    }
+    },
   },
   methods: {
-    subIsActive (paths) {
+    subIsActive(paths) {
       const activePaths = Array.isArray(paths) ? paths : [paths]
 
-      return activePaths.some(path => {
+      return activePaths.some((path) => {
         return this.$route.path.indexOf(path) === 0 // current path starts with this path string
       })
     },
-    linkClicked (e, submenu) {
+    linkClicked(e, submenu) {
       // Get window width
-      let windowW = window.innerWidth || document.documentElement.clientWidth || document.body.clientWidth
+      let windowW =
+        window.innerWidth ||
+        document.documentElement.clientWidth ||
+        document.body.clientWidth
 
       if (submenu) {
         // Get closest li element
@@ -115,20 +139,15 @@ export default {
             el.classList.remove('open')
           } else {
             // .. else if submenu is closed, close all other (same level) submenus first before open it
-            Array.from(el.closest('ul').children).forEach(element => {
+            Array.from(el.closest('ul').children).forEach((element) => {
               element.classList.remove('open')
             })
 
             el.classList.add('open')
           }
         }
-      } else {
-        // If we are in mobile, close the sidebar
-        if (windowW < 992) {
-          this.$store.commit('sidebar', { mode: 'close' })
-        }
       }
-    }
-  }
+    },
+  },
 }
 </script>
