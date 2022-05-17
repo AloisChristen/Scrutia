8b5d34ffd8b761377f3ccd6e0b942cb207ef363c
Hakim Balestrieri <hakimbalestrieri@gmail.com>
2022-05-17 21:33:02 +0200

    fix test

--- scrutia-api/tests/Feature/ProjectTest.php
+++ scrutia-api/tests/Feature/ProjectTest.php
@@ -51,7 +51,7 @@ class ProjectTest extends TestCase
     public function test_can_get_project_by_created_at_filtered_by_startDate_and_endDate(): void
     {
         $project = Project::factory()->create(['created_at' => '2020-01-02']);
-        $this->assertEquals('2020-01-01', Carbon::parse($project->created_at)
+        $this->assertEquals('2020-01-02', Carbon::parse($project->created_at)
             ->format('Y-m-d'));
 
         $response = $this->get('/api/projects?filter[startDate]=2020-01-01&filter[endDate]=2020-01-31');
