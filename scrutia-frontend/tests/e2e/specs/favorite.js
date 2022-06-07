2b4901190efd0bfca7e80ef28aee6eb31419b26e
AloisChristen <alois.christen@heig-vd.ch>
2022-06-07 11:21:35 +0200

    avancé tests

--- scrutia-frontend/tests/e2e/specs/favorite.js
+++ scrutia-frontend/tests/e2e/specs/favorite.js
@@ -1,4 +1,3 @@
-/*
 const uuid = "" + Cypress._.random(0, 100) + Date.now()
 
 const credentials = {
@@ -6,30 +5,59 @@ const credentials = {
   password: "password_" + uuid,
   email: "email" + uuid + "@emailtest.ch"
 }
-*/
+
 
 
 describe("Authentification test", () => {
-  describe("Create user for tests", () => {
-    // Create account
+  before(() => {
+    // Create test user
+    cy.visit('/auth/signup')
+    cy.contains('label', "J'accepte les Termes & Conditions").click();
+      cy.get('input[name="username"').type(credentials.username);
+      cy.get('input[name="email"').type(credentials.email);
+      cy.get('input[name="password"').type(credentials.password);
+      cy.get('input[name="password_confirmation"').type(credentials.password);
+      cy.get('input[name="firstname"').type("testfirstname")
+      cy.get('input[name="lastname"').type("testlastname")
+      // submit
+      cy.contains("button", "S'inscrire").click()
+      cy.url().should('include', '/home')
   })
 
+
+
   describe("Base test", () => {
+    beforeEach(() => {
+      // Sign in
+      cy.visit('/auth/signin')
+      cy.get('input[name="username"').type(credentials.username);
+      cy.get('input[name="password"').type(credentials.password);
+      // submit
+      cy.contains("button", "Se connecter").click()
+    })
     it("Visits the app root url", () => {
       cy.visit("/");
       cy.title().should("eq", "Scrutia");
       cy.contains("a", "Accueil").should("have.class", "active");
     });
     it("Visits the favorite page", () => {
-      cy.visit("/favorites");
-      // check base layout
+      cy.get("#userProfile").click();
+      cy.contains("a", "Mes favoris").should("have.attr", "href", "/favorites").click();
+      cy.contains("h2", "Vos idées et projets favoris")
+      cy.contains("div", "Vous n'avez actuellement pas de favoris.")
     });
   })
 
   describe("Happy scenario", () => {
-
+    beforeEach(() => {
+      // Sign in
+      cy.visit('/auth/signin')
+      cy.get('input[name="username"').type(credentials.username);
+      cy.get('input[name="password"').type(credentials.password);
+      // submit
+      cy.contains("button", "Se connecter").click()
+    })
     it("Favorites some projects", () => {
-      // Go to main page
       // Search for projects
       // Favorites them
     })
